a530cc72681b6569f0ac0c2da529c87fdd3c339a
ingjuangalvan <camik200@hotmail.com>
2026-03-13 21:04:21 -0500

    Agregar tareas completadas

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-function saveTasks() {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+function saveTasks(){
+localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-function renderTasks() {
-    const list = document.getElementById('taskList');
-    list.innerHTML = '';
+function renderTasks(){
 
-    tasks.forEach((task, index) => {
-        const li = document.createElement('li');
+const list = document.getElementById("taskList");
+list.innerHTML="";
 
-        li.innerHTML = `
-        ${task}
-        <button class="delete" onclick="deleteTask(${index})">X</button>`;
-        list.appendChild(li);
-    });
+tasks.forEach((task,index)=>{
+
+const li = document.createElement("li");
+
+li.innerHTML = `
+<span class="${task.completed ? 'completed' : ''}" onclick="toggleTask(${index})">
+${task.text}
+</span>
+
+<button class="delete" onclick="deleteTask(${index})">X</button>
+`;
+
+list.appendChild(li);
+
+});
 }
 
-function addTask() {
-    const input = document.getElementById('taskInput');
-    const task = input.value.trim();
+function addTask(){
 
-    if (!task) {
-        alert("Escribe una tarea antes de agregarla.");
-        input.focus();
-        return;
-    }
-    tasks.push(task);
-    saveTasks
-    renderTasks();
-    input.value = '';
+const input = document.getElementById("taskInput");
+const text = input.value.trim();
+
+if(!text){
+alert("Debes escribir una tarea");
+return;
 }
 
-function deleteTask(index) {
-    tasks.splice(index, 1);
-    saveTasks();
-    renderTasks();
-}
+tasks.push({
+text:text,
+completed:false
+});
+
+saveTasks();
 renderTasks();
 
-document.getElementById("taskInput").addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        addTask();
-    }
-});
+input.value="";
+}
+
+function deleteTask(index){
+
+tasks.splice(index,1);
+
+saveTasks();
+renderTasks();
+}
+
+function toggleTask(index){
+
+tasks[index].completed = !tasks[index].completed;
+
+saveTasks();
+renderTasks();
+}
+
+renderTasks();
